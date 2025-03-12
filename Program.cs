d46b68225cb2d6d9884da03a3de0b53d39f01640
Bartechnika <steamp4nk@gmail.com>
2025-03-12 00:07:17 +0000

    Update 1.0

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace StringTutorial
 {
@@ -11,15 +12,15 @@ namespace StringTutorial
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a string to encrypt:");
-            
+            string plaintext = Console.ReadLine();
 
             try
             {
-                
+                Console.WriteLine($"The encrypted message is {EncryptString(plaintext)}");
             }
-            catch ()
+            catch (ArgumentNullException ex)
             {
-                  
+                  Console.WriteLine($"Error: {ex.Message}");
             }
             finally
             {
@@ -29,22 +30,38 @@ namespace StringTutorial
         }
 
         static string EncryptString(string inputString)
-        {
+        {   
             // Guard clause to check if input is a valid string
-            
+            if (string.IsNullOrWhiteSpace(inputString))
+            {
+                throw new ArgumentNullException(nameof(inputString));
+            }
+
             // Reverse the string
-            
+            inputString = inputString.Trim();
+
+            char[] array = inputString.ToCharArray();
+            Array.Reverse(array);
 
             // Convert every second charatcer to uppercase
-           
+            for (int i = 0; i<inputString.Length; i++)
+            {
+                if(i%2 == 0)
+                {
+                    array[i] = Char.ToUpper(array[i]);
+                }
+            }
 
             // Interpolateion and concatenation
-          
+            inputString = new string(array);
+            inputString = "Secret-" + inputString + "-Code";
 
             // String conversion using ASCII values to shift each character by 1
-            
-            
-            string finalEncryption = new string(finalEncryptionChars);
+            array = inputString.ToCharArray();
+            var newArray = array.Select(x => (char)((int)(x + 1)));
+            array = newArray.ToArray();
+
+            string finalEncryption = new string(array);
             return finalEncryption;
         }
     }
